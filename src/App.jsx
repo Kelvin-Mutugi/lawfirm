89d7eeb965037c47bd2295a811a78cfdcf88233d
Kelvin-Mutugi <mutugikelvin316@gmail.com>
2026-03-12 19:35:50 +0300

    version checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/navbar'
@@ -9,7 +10,29 @@ import Team from './pages/Team'
 import Contact from './pages/Contact'
 import BookConsultation from './pages/BookConsultation'
 
+
+// App version (must match version.json)
+const APP_VERSION = "1.0.1";
+
+
 function App() {
+  
+    // Version check + force refresh
+  useEffect(() => {
+    fetch("/version.json", { cache: "no-store" })
+      .then(res => res.json())
+      .then(data => {
+        if (data.version !== APP_VERSION) {
+          window.location.reload(true);
+          console.log("New version detected, refreshing...");
+        }
+      })
+      .catch(() => {
+        console.log("Version check failed");
+      });
+  }, []);
+
+
   return (
     <Router>
       <Navbar />
